fa429146afec696d2642fa7be418b01266396c9d
caengcjd <1206557575@qq.com>
2015-07-01 14:15:45 +0800

    add logout

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -22,6 +22,7 @@ class UserController extends BaseController {
 	        $user->school;
 	        $user->major;
 	        $user->tags;
+                
 	        return $user;
 	    }else{
 	        return json_encode(array('error'=>1, 'msg'=>'no user'));
@@ -36,12 +37,22 @@ class UserController extends BaseController {
 	    $user = User::create($data);
 	    return $user->toJson();
 	}
-	
+	public function is_md5($password) {
+                // echo 'ans is '.preg_match("/^[a-z0-9]{32}$/",$password);
+		return preg_match("/^[a-z0-9]{32}$/", $password);
+        }
 	// PUT /user/$id
 	public function update($id)
 	{
-	    $user = User::find($id);
-	    $user->update(Input::get());
+	    $user = User::find($id);/*
+              echo 'ans is '.preg_match("/^[a-z0-9]{32}$/",Input::get('password'));
+          */
+
+
+           $data=Input::get();
+            $va=preg_match("/^[a-z0-9]{32}$/",(Input::get('password')));
+	    if(!$va){$data['password']=md5(Input::get('password'));}
+	    $user->update($data);
 	    return $user;
 	}
 	public function destroy($id){
@@ -61,7 +72,16 @@ class UserController extends BaseController {
 	        return $this->outputError('login error');
 	    }
 	}
-	
+	public function logout(){
+         
+        if(Session::has('uid')){
+           Session::forget('uid');  
+           //echo 'you have logout';
+           return  $this->output('logout');
+         }else {
+           return $this->output('you are not login');
+        }
+        }//logout
 	public function session()
 	{
 	    if (Session::has('uid')){
--- app/routes.php
+++ app/routes.php
@@ -10,37 +10,34 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-Route::resource('version', 'VersionController');
-Route::resource('vatstr', 'VatstrController');
-Route::resource('user', 'UserController');
-Route::get('treevat/root', 'TreeVatController@root');
-Route::resource('treevat','TreeVatController');
-Route::get('treemod', 'TreeController@treemod');
-Route::get('treeitem/root', 'TreeController@root');
-Route::resource('treeitem', 'TreeItemController');
-Route::get('tree/root', 'TreeController@root');
-Route::resource('tree', 'TreeController');
-Route::resource('testmethod', 'TestmethodController');
 Route::get('/test', 'TestController@index');
 Route::get('tc/export', 'TcController@export');
+Route::resource('project', 'ProjectController');
+
+Route::get('tc/maxtag', 'TcController@maxtag');
+Route::resource('rs', 'RsController');
 Route::resource('tc', 'TcController');
-Route::get('testjob/rsversion', 'TestjobController@rsversion');
-Route::get('testjob/export', 'TestjobController@export');
-Route::resource('testjob', 'TestjobController');
+Route::resource('document', 'DocumentController');
+Route::resource('document.rs', 'DocumentRsController');
+Route::get('tree/root', 'TreeController@root');
+Route::resource('tree', 'TreeController');
+Route::get('treeitem/root', 'TreeController@root');
+Route::resource('treeitem', 'TreeItemController');
+Route::get('treemod', 'TreeController@treemod');
+Route::post('docfile', 'ProjectController@docfile');
+Route::resource('vatstr', 'VatstrController');
+Route::resource('user', 'UserController');
+Route::resource('testmethod', 'TestmethodController');
+Route::get('dump', 'DumpController@dump');
+Route::resource('build', 'BuildController');
+//Route::get('version', 'DocumentController@version');
 Route::get('stat/count', 'StatController@count');
 Route::post('setresult', 'TcController@setresult');
-Route::get('session', 'UserController@session');
-Route::resource('rs', 'RsController');
-Route::put('result/updateall', 'ResultController@updateall');
-Route::resource('result', 'ResultController');
-Route::resource('project', 'ProjectController');
-Route::post('login', 'UserController@login');
 Route::get('fix', 'FixController@index');
-Route::get('dump', 'DumpController@dump');
-Route::resource('document.rs', 'DocumentRsController');
-Route::resource('document', 'DocumentController');
-Route::post('docfile', 'ProjectController@docfile');
-Route::resource('build', 'BuildController');
+Route::resource('version', 'VersionController');
 
-
-
+Route::resource('testjob', 'TestjobController');
+Route::get('session', 'UserController@session');
+Route::resource('result', 'ResultController');
+Route::post('login', 'UserController@login');
+Route::get('logout','UserController@logout');
