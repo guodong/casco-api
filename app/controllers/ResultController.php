a8708e0143986d78c19375bdeac48123ff6ff123
guodong <gd@tongjo.com>
2015-08-03 10:48:21 +0800

    d

--- app/controllers/ResultController.php
+++ app/controllers/ResultController.php
@@ -15,10 +15,13 @@ class ResultController extends BaseController{
 	    foreach ($results as $v){
 	        $sr = json_decode($v->step_result_json)?json_decode($v->step_result_json):[];
 	        foreach ($v->tc->steps as $step){
-	            $step->result = 0;
-	            if (key_exists($step->id, $sr)){
-	                $step->result = $sr[$step->id];
+	            $sr = ResultStep::where('result_id', $v->id)->where('step_id', $step->id)->first();
+	            if(!$sr){
+	                $sr = ResultStep::create(array('result_id'=>$v->id, 'step_id'=>$step->id, 'result'=>0, 'comment'=>''));
 	            }
+	            $step->result = $sr->result;
+	            $step->comment = $sr->comment;
+	            $step->step_result_id = $sr->id;
 	        }
 	        
 	        $arr = explode(',',$v->tc->testmethod_id);
@@ -32,6 +35,33 @@ class ResultController extends BaseController{
 	        $v->tc->testmethods = $tms;
 	    }
 	    return $results;
+	}
+	
+	public function update()
+	{
+	    Result::update(Input::get());
+	}
+	
+	public function updateall()
+	{
+	    foreach (Input::get('results') as $v){
+	        $r = Result::find($v['id']);
+	        $r->begin_at = $v['begin_at'];
+	        $r->end_at = $v['end_at'];
+	        $r->result = $v['result'];
+	        $r->cr = $v['cr'];
+	        $r->save();
+	        foreach ($v['steps'] as $vv){
+	            $s = ResultStep::find($vv['step_result_id']);
+	            if(!$s){
+	                $s = ResultStep::create(array('result_id'=>$r['id'],'step_id'=>$vv['id'],'result'=>$vv['result'],'comment'=>$vv['comment']));
+	            }else{
+	                $s->result = $vv['result'];
+	                $s->comment = $vv['comment'];
+	                $s->save();
+	            }
+	        }
+	    }
 	}
 	
 }
---
+++ app/models/ResultStep.php
@@ -0,0 +1,13 @@
+<?php
+class ResultStep extends BaseModel {
+
+	protected $table = 'result_step';
+	protected $fillable = array('result_id', 'step_id', 'result', 'comment');
+
+	public function step()
+	{
+	   return $this->belongsTo('Step');
+	}
+
+
+}
