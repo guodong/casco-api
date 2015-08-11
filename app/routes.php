4d49e86c4001a8a97e737e1de2b6415c4fa75d99
guodong <gd@tongjo.com>
2015-08-11 11:13:24 +0800

    d

--- app/routes.php
+++ app/routes.php
@@ -10,34 +10,34 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-Route::get('/test', 'TestController@index');
-Route::get('tc/export', 'TcController@export');
-Route::resource('project', 'ProjectController');
-
-Route::resource('rs', 'RsController');
-Route::resource('tc', 'TcController');
-Route::resource('document', 'DocumentController');
-Route::resource('document.rs', 'DocumentRsController');
-Route::get('tree/root', 'TreeController@root');
-Route::resource('tree', 'TreeController');
-Route::get('treeitem/root', 'TreeController@root');
-Route::resource('treeitem', 'TreeItemController');
-Route::get('treemod', 'TreeController@treemod');
-Route::post('docfile', 'ProjectController@docfile');
+Route::resource('version', 'VersionController');
 Route::resource('vatstr', 'VatstrController');
 Route::resource('user', 'UserController');
+Route::get('treemod', 'TreeController@treemod');
+Route::get('treeitem/root', 'TreeController@root');
+Route::resource('treeitem', 'TreeItemController');
+Route::get('tree/root', 'TreeController@root');
+Route::resource('tree', 'TreeController');
 Route::resource('testmethod', 'TestmethodController');
-Route::get('dump', 'DumpController@dump');
-Route::resource('build', 'BuildController');
-//Route::get('version', 'DocumentController@version');
+Route::get('/test', 'TestController@index');
+Route::get('tc/export', 'TcController@export');
+Route::resource('tc', 'TcController');
+Route::get('testjob/rsversion', 'TestjobController@rsversion');
+Route::get('testjob/export', 'TestjobController@export');
+Route::resource('testjob', 'TestjobController');
 Route::get('stat/count', 'StatController@count');
 Route::post('setresult', 'TcController@setresult');
-Route::get('fix', 'FixController@index');
-Route::resource('version', 'VersionController');
-
-Route::get('testjob/rsversion', 'TestjobController@rsversion');
-Route::resource('testjob', 'TestjobController');
 Route::get('session', 'UserController@session');
+Route::resource('rs', 'RsController');
 Route::put('result/updateall', 'ResultController@updateall');
 Route::resource('result', 'ResultController');
+Route::resource('project', 'ProjectController');
 Route::post('login', 'UserController@login');
+Route::get('fix', 'FixController@index');
+Route::get('dump', 'DumpController@dump');
+Route::resource('document.rs', 'DocumentRsController');
+Route::resource('document', 'DocumentController');
+Route::post('docfile', 'ProjectController@docfile');
+Route::resource('build', 'BuildController');
+
+
